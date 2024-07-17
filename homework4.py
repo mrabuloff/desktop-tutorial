920a856918a3979c3927ce1228a0dae3c0072f5c
mrabuloff <109786928+mrabuloff@users.noreply.github.com>
2024-07-17 12:03:57 +0300

    Create homework4.py
    Практическая работа по уроку "Организация программ и методы строк."
    
    Цель: Написать программу на языке Python с использованием Pycharm для работы с методами строк и организации программ.
    
    1. В проекте, где вы решаете домашние задания, создайте модуль 'homework4.py' и напишите весь код в нём.
    
    2. Организуйте программу:
    Создайте переменную my_string и присвойте ей значение строки с произвольным текстом (функция input()).
    Вывести количество символов введённого текста
    3. Работа с методами строк:
    Используя методы строк, выполните следующие действия:
    Выведите строку my_string в верхнем регистре.
    Выведите строку my_string в нижнем регистре.
    Измените строку my_string, удалив все пробелы.
    Выведите первый символ строки my_string.
    Выведите последний символ строки my_string.

---
+++ homework4.py
@@ -0,0 +1,6 @@
+my_string=input('Введите любой текст:')
+print(my_string.upper())    # Выводим строку в верхнем регистре
+print(my_string.lower())    # Выводим строку в нижнем регистре
+print(my_string.replace(' ', '')) # Удаление всех пробелов
+print(my_string[0])     # Вывожу первый символ строки my_string
+print(my_string[-1])    # Вывожу последний символ строки my_string
