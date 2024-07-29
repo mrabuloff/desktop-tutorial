0b20dd9db4b19da63fb28f5659acc49d3d893507
mrabuloff <109786928+mrabuloff@users.noreply.github.com>
2024-07-29 11:29:46 +0300

    Update module_2_4.py

--- module_2_4.py
+++ module_2_4.py
@@ -12,7 +12,7 @@
 Выведите списки primes и not_primes на экран(в консоль)."""
 
 
-numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+"""numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 primes = []
 not_primes = []
 
@@ -28,4 +28,27 @@ for i in numbers:
         not_primes.append(i)
 
 print(primes)
-print(not_primes)
+print(not_primes)"""
+
+
+for num in numbers:
+    if num == 1:
+        continue
+    is_prime = True
+    for i in range(2, int(num ** 0.5) + 1):
+        if num % i == 0:
+            is_prime = False
+            break
+    if is_prime:
+        primes.append(num)
+    else:
+        not_primes.append(num)
+
+print("Primes:", primes)
+print("Not Primes:", not_primes)
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+primes = []
+not_primes = []
+
+
