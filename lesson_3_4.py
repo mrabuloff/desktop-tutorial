b17e3a8de4b9b9b0cc2bd86f6fc0b00f810ec42c
mrabuloff <mr.abuloff@mail.ru>
2024-08-09 13:27:46 +0300

    3 модуль

--- lesson_3_4.py
+++ lesson_3_4.py
@@ -1,38 +1,26 @@
-# def test_func(*params):
-#     print("Тип:", type(params))
-#     print("Аргумент:", params)
-#
-# #
-# # test_func(1, 2, 3, 4)
-#
-# def summator(txt, *values, type="sum"):
-#     s = 0
-#     for i in values:
-#         s += i
-#     return f'{txt}{s} {type}'
-#
-#
-#
-# print(summator("Сумма чисел: ", 2, 3, 4, type="summator"))
+"""Задача "Однокоренные":
+Напишите функцию single_root_words, которая принимает одно обязательное слово в параметр root_word, а далее неограниченную последовательность в параметр *other_words.
+Функция должна составить новый список same_words только из тех слов списка other_words, которые содержат root_word или наоборот root_word содержит одно из этих слов. После вернуть список same_words в качестве результата своей работы.
+
+Пункты задачи:
+Объявите функцию single_root_words и напишите в ней параметры root_word и *other_words.
+Создайте внутри функции пустой список same_words, который пополнится нужными словами.
+При помощи цикла for переберите предполагаемо подходящие слова.
+Пропишите корректное относительно задачи условие, при котором добавляются слова в результирующий список same_words.
+После цикла верните образованный функцией список same_words.
+Вызовите функцию single_root_words и выведете на экран(консоль) возвращённое ей значение."""
+
+def single_root_words(root_word, *other_words):
+    same_words = []
+    root_word_lower = root_word.lower()
+    for i in other_words:
+        other_words_lower = i.lower()
+        if root_word_lower in other_words_lower or other_words_lower in root_word_lower:
+            same_words.append(i)
+    return same_words
 
 
-def info(value, *types, names_author="Den", **values):
-    print("Тип:", type(values))
-    print("Аргумент:", values)
-    for key, value in values.items():
-        print(key, value)
-    print(types)
-
-
-info("Пример использования параметров всех типов", 2, 3, 4, names_author="Denis", name="Den", course="Python")
-
-
-def my_sum(n, *args, txt="Сумма чисел"):
-    s = 0
-    for i in range(len(args)):
-        s += args[i] ** n
-    print(txt + ":", s)
-
-
-my_sum(1, 1, 2, 3, 4, 5)
-my_sum(2, 2, 3, 4, 5, txt="Сумма квадратов")
+result1 = single_root_words("rich", "richiest", "orichalcum", "cheers", "richies")
+result2 = single_root_words("Disablement", "Able", "Mable", "Disable", "Bagel")
+print(result1)
+print(result2)
