20af5ac3a6abe70573eb6d59f4705c0882b6d08c
mrabuloff <mr.abuloff@mail.ru>
2024-08-09 13:10:15 +0300

    3 модуль

--- module_3_3.py
+++ module_3_3.py
@@ -12,8 +12,21 @@
 Создайте список values_list_2 с двумя элементами разных типов
 Проверьте, работает ли print_params(*values_list_2, 42)"""
 
-def print_params(a = 1, b = 'строка', c = True):
-    print(a, b, c)  # 1 строка True
+#  Распаковка позиционных параметров
+def print_params(a=1, b='строка', c=True):
+    print(a, b, c)
+    return
 
-values_list = [54.32, 'Строка' ]
-print_params(*values_list, 42)
+#  1.Функция с параметрами по умолчанию:
+print_params(b=25)
+print_params(c=[1, 2, 3])
+
+#  2.Распаковка параметров:
+values_list = [12, 'students', False]
+print_params(*values_list)
+values_dict = {'a': 'Vlad', 'b': 'Urban', 'c': 2024}
+print_params(**values_dict)
+
+#  3.Распаковка + отдельные параметры:
+values_list_2 = [54.32, 'Строка' ]
+print_params(*values_list_2, 42)
