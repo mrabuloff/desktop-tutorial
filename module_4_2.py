9f2df9702019cd9186263483c320981c7b74f216
mrabuloff <mr.abuloff@mail.ru>
2024-08-15 14:42:43 +0300

    4 модуль

---
+++ module_4_2.py
@@ -0,0 +1,16 @@
+# Домашнее задание по уроку "Пространство имен"
+#
+# Создайте новый проект в PyCharm
+# Запустите созданный проект
+# Ваша задача:
+# Создайте новую функцию def test_function
+# Создайте другую функцию внутри функции inner_function, функция должна печатать значение "Я в области видимости функции test_function"
+# Вызовите функцию inner_function внутри функции test_function
+# Попробуйте вызывать inner_function вне функции test_function и посмотрите на результат выполнения программы
+
+def test_function():
+    def inner_function():
+        print("Я в области видимости функции test_function")
+    inner_function()
+
+test_function()
