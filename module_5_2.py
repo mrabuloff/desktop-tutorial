5e7ec558d67ca27acc0b642bfc90ce48da001c90
mrabuloff <mr.abuloff@mail.ru>
2024-08-19 13:29:33 +0300

    5 модуль

--- module_5_2.py
+++ module_5_2.py
@@ -22,6 +22,7 @@ class House:
 
     def __str__(self):
         return f"Название: {self.name}, кол-во этажей: {self.number_of_floors}"
+
 h1 = House('ЖК Эльбрус', 10)
 h2 = House('ЖК Акация', 20)
 
