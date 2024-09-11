57875b6d8b9322473870c9c6f5f3b981c67fa6fe
mrabuloff <mr.abuloff@mail.ru>
2024-09-11 15:25:40 +0300

    5 модуль

---
+++ module_5_4.py
@@ -0,0 +1,99 @@
+# Задача "История строительства":
+# Для решения этой задачи будем пользоваться решением к предыдущей задаче "Перегрузка операторов".
+#
+# В классе House создайте атрибут houses_history = [], который будет хранить названия созданных объектов.
+#
+# Правильней вписывать здание в историю сразу при создании объекта, тем более можно удобно обращаться к атрибутам класса используя ссылку на сам класс - cls.
+# Дополните метод __new__ так, чтобы:
+# Название объекта добавлялось в список cls.houses_history.
+# Название строения можно взять из args по индексу.
+#
+# Также переопределите метод __del__(self) в котором будет выводиться строка:
+# "<название> снесён, но он останется в истории"
+#
+# Создайте несколько объектов класса House и проверьте работу методов __del__ и __new__, а также значение атрибута houses_history.
+
+class House:
+
+    houses_history = []
+
+    def __new__(cls, *args, **kwargs):
+        instance = object.__new__(cls)
+        args = args[0]
+        cls.houses_history.append(args)
+        return instance
+
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+        if isinstance(number_of_floors, House):
+            self.houses_history = number_of_floors.append()
+
+    def go_to(self, new_floor):
+        if new_floor <= self.number_of_floors:
+            for i in range(1, new_floor + 1):
+                print(i)
+        else:
+            print("Такого этажа не существует")
+
+    def __del__(self):
+        return print(f'{self.name} снесён, но он останется в истории')
+
+    def __len__(self):
+        return self.number_of_floors
+
+    def __str__(self):
+        return f"Название: {self.name}, количество этажей: {self.number_of_floors}"
+
+    def __eq__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors == other.number_of_floors
+        return False
+
+    def __lt__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors < other.number_of_floors
+        return NotImplemented
+
+    def __le__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors <= other.number_of_floors
+        return NotImplemented
+
+    def __gt__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors > other.number_of_floors
+        return NotImplemented
+
+    def __ge__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors >= other.number_of_floors
+        return NotImplemented
+
+    def __ne__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors != other.number_of_floors
+        return True
+
+    def __add__(self, value):
+        if isinstance(value, int):
+            return House(self.name, self.number_of_floors + value)
+        return NotImplemented
+
+    def __radd__(self, value):
+        return self.__add__(value)
+
+
+
+h1 = House('ЖК Эльбрус', 10)
+print(House.houses_history)
+h2 = House('ЖК Акация', 20)
+print(House.houses_history)
+h3 = House('ЖК Матрёшки', 20)
+print(House.houses_history)
+
+# Удаление объектов
+del h2
+del h3
+
+print(House.houses_history)
